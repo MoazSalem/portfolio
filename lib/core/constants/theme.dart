f6be4ac2568c01259e8de544f1c24f28598e028d
Moaz Talaat <88838071+MoazSalem@users.noreply.github.com>
2025-10-13 13:42:56 +0300

    Preloading animations to avoid blank white screen

--- lib/core/constants/theme.dart
+++ lib/core/constants/theme.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
+  /// Don't forget to update initial loading screen colors in web/index.html
   static final ThemeData darkTheme = ThemeData(
     colorScheme: ColorScheme.fromSeed(
       seedColor: Colors.cyanAccent,
