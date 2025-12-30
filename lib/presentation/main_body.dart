5e18269db59c88b825d877c3386393e7987efb98
Moaz Talaat <moaztalaat6@gmail.com>
2025-12-30 17:43:30 +0200

    smooth scrolling with the mouse

--- lib/presentation/main_body.dart
+++ lib/presentation/main_body.dart
@@ -8,6 +8,7 @@ import 'package:portfolio/presentation/feature/independent_animations/intro_anim
 import 'package:portfolio/presentation/feature/introduction_module/module/introduction_module.dart';
 import 'package:portfolio/presentation/feature/projects_module/module/projects_module.dart';
 import 'package:portfolio/presentation/shared_widgets/modules_link.dart';
+import 'package:scroll_animator/scroll_animator.dart';
 
 import 'feature/contact_module/module/contact_module.dart';
 
@@ -19,7 +20,6 @@ class MainBody extends StatefulWidget {
 }
 
 class _MainBodyState extends State<MainBody> {
-  late ScrollController _scrollController;
   // A state variable to control visibility
   bool _showAnimation = true;
 
@@ -28,7 +28,6 @@ class _MainBodyState extends State<MainBody> {
   @override
   void initState() {
     super.initState();
-    _scrollController = ScrollController();
     // Start a timer when the widget is first built
     Future.delayed(_animationDuration, () {
       // When the timer completes, update the state to hide the animation
@@ -41,90 +40,89 @@ class _MainBodyState extends State<MainBody> {
   }
 
   @override
-  void dispose() {
-    super.dispose();
-    _scrollController.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     // This stack is used to position animations over modules
-    return CupertinoScrollbar(
-      controller: _scrollController,
-      child: Center(
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            SelectionArea(
-              child: ConstrainedBox(
-                constraints: const BoxConstraints(
-                  maxWidth: AppSizes.moduleMaxWidth,
-                  minWidth: AppSizes.moduleMinWidth,
-                ),
-                child: CustomScrollView(
-                  controller: _scrollController,
-                  scrollBehavior: const ScrollBehavior().copyWith(
-                    scrollbars: false,
+    return AnimatedPrimaryScrollController(
+      animationFactory: const ChromiumEaseInOut(),
+      child: CupertinoScrollbar(
+        child: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              SelectionArea(
+                child: ConstrainedBox(
+                  constraints: const BoxConstraints(
+                    maxWidth: AppSizes.moduleMaxWidth,
+                    minWidth: AppSizes.moduleMinWidth,
                   ),
-                  slivers: [
-                    SliverToBoxAdapter(
-                      child: IntroductionModule(
-                        introductionData: user.introductionData,
-                        externalLinks: user.externalLinks,
+                  child: CustomScrollView(
+                    scrollBehavior: const ScrollBehavior().copyWith(
+                      scrollbars: false,
+                    ),
+                    physics: const ClampingScrollPhysics(),
+                    slivers: [
+                      SliverToBoxAdapter(
+                        child: IntroductionModule(
+                          introductionData: user.introductionData,
+                          externalLinks: user.externalLinks,
+                        ),
                       ),
-                    ),
-                    const SliverToBoxAdapter(child: ModulesLink()),
-                    SliverToBoxAdapter(
-                      child: EducationModule(educationData: user.educationData),
-                    ),
-                    const SliverToBoxAdapter(child: ModulesLink()),
-                    SliverLayoutBuilder(
-                      builder: (context, constraints) {
-                        return DecoratedSliver(
-                          decoration: BoxDecoration(
-                            color: theme.colorScheme.surfaceContainer,
-                          ),
-                          sliver: SliverPadding(
-                            padding: const EdgeInsets.symmetric(
-                              vertical: AppSizes.moduleInnerVerticalPadding,
-                              horizontal: AppSizes.moduleInnerHorizontalPadding,
+                      const SliverToBoxAdapter(child: ModulesLink()),
+                      SliverToBoxAdapter(
+                        child: EducationModule(
+                          educationData: user.educationData,
+                        ),
+                      ),
+                      const SliverToBoxAdapter(child: ModulesLink()),
+                      SliverLayoutBuilder(
+                        builder: (context, constraints) {
+                          return DecoratedSliver(
+                            decoration: BoxDecoration(
+                              color: theme.colorScheme.surfaceContainer,
                             ),
-                            sliver: SliverMainAxisGroup(
-                              slivers: [
-                                const SliverToBoxAdapter(
-                                  child: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      ProjectsHeader(),
-                                      SizedBox(height: AppSizes.p60),
-                                    ],
+                            sliver: SliverPadding(
+                              padding: const EdgeInsets.symmetric(
+                                vertical: AppSizes.moduleInnerVerticalPadding,
+                                horizontal:
+                                    AppSizes.moduleInnerHorizontalPadding,
+                              ),
+                              sliver: SliverMainAxisGroup(
+                                slivers: [
+                                  const SliverToBoxAdapter(
+                                    child: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        ProjectsHeader(),
+                                        SizedBox(height: AppSizes.p60),
+                                      ],
+                                    ),
                                   ),
-                                ),
-                                ProjectsSliverGrid(projects: user.projects),
-                              ],
+                                  ProjectsSliverGrid(projects: user.projects),
+                                ],
+                              ),
                             ),
-                          ),
-                        );
-                      },
-                    ),
-                    const SliverToBoxAdapter(child: ModulesLink()),
-                    SliverToBoxAdapter(
-                      child: ContactModule(
-                        email: user.email,
-                        externalLinks: user.externalLinks.reversed.toList(),
+                          );
+                        },
                       ),
-                    ),
-                  ],
+                      const SliverToBoxAdapter(child: ModulesLink()),
+                      SliverToBoxAdapter(
+                        child: ContactModule(
+                          email: user.email,
+                          externalLinks: user.externalLinks.reversed.toList(),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            if (_showAnimation) // to unmount the animations once it's done
-              const IntroAnimation(
-                rectWidth: AppSizes.introAnimationRectWidth,
-                duration: _animationDuration,
-              ),
-          ],
+              if (_showAnimation) // to unmount the animations once it's done
+                const IntroAnimation(
+                  rectWidth: AppSizes.introAnimationRectWidth,
+                  duration: _animationDuration,
+                ),
+            ],
+          ),
         ),
       ),
     );
