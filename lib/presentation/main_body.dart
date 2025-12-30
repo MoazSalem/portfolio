facbfa0598fab69f7bd73ff579e0f5e59c56d864
Moaz Talaat <moaztalaat6@gmail.com>
2025-12-30 17:17:17 +0200

    avoid repetition with width constrains

--- lib/presentation/main_body.dart
+++ lib/presentation/main_body.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:portfolio/core/constants/user.dart';
 import 'package:portfolio/core/theme/durations.dart';
@@ -18,6 +19,7 @@ class MainBody extends StatefulWidget {
 }
 
 class _MainBodyState extends State<MainBody> {
+  late ScrollController _scrollController;
   // A state variable to control visibility
   bool _showAnimation = true;
 
@@ -26,6 +28,7 @@ class _MainBodyState extends State<MainBody> {
   @override
   void initState() {
     super.initState();
+    _scrollController = ScrollController();
     // Start a timer when the widget is first built
     Future.delayed(_animationDuration, () {
       // When the timer completes, update the state to hide the animation
@@ -38,134 +41,92 @@ class _MainBodyState extends State<MainBody> {
   }
 
   @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     // This stack is used to position animations over modules
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        SelectionArea(
-          child: CustomScrollView(
-            slivers: [
-              SliverToBoxAdapter(
-                child: Center(
-                  child: ConstrainedBox(
-                    constraints: const BoxConstraints(
-                      maxWidth: AppSizes.moduleMaxWidth,
-                      minWidth: AppSizes.moduleMinWidth,
-                    ),
-                    child: IntroductionModule(
-                      introductionData: user.introductionData,
-                      externalLinks: user.externalLinks,
-                    ),
-                  ),
+    return CupertinoScrollbar(
+      controller: _scrollController,
+      child: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            SelectionArea(
+              child: ConstrainedBox(
+                constraints: const BoxConstraints(
+                  maxWidth: AppSizes.moduleMaxWidth,
+                  minWidth: AppSizes.moduleMinWidth,
                 ),
-              ),
-              SliverToBoxAdapter(
-                child: Center(
-                  child: ConstrainedBox(
-                    constraints: const BoxConstraints(
-                      maxWidth: AppSizes.moduleMaxWidth,
-                      minWidth: AppSizes.moduleMinWidth,
-                    ),
-                    child: const ModulesLink(),
+                child: CustomScrollView(
+                  controller: _scrollController,
+                  scrollBehavior: const ScrollBehavior().copyWith(
+                    scrollbars: false,
                   ),
-                ),
-              ),
-              SliverToBoxAdapter(
-                child: Center(
-                  child: ConstrainedBox(
-                    constraints: const BoxConstraints(
-                      maxWidth: AppSizes.moduleMaxWidth,
-                      minWidth: AppSizes.moduleMinWidth,
-                    ),
-                    child: EducationModule(educationData: user.educationData),
-                  ),
-                ),
-              ),
-              SliverToBoxAdapter(
-                child: Center(
-                  child: ConstrainedBox(
-                    constraints: const BoxConstraints(
-                      maxWidth: AppSizes.moduleMaxWidth,
-                      minWidth: AppSizes.moduleMinWidth,
-                    ),
-                    child: const ModulesLink(),
-                  ),
-                ),
-              ),
-              SliverLayoutBuilder(
-                builder: (context, constraints) {
-                  final double horizontalPadding =
-                      (constraints.crossAxisExtent - AppSizes.moduleMaxWidth) /
-                      2;
-                  final double effectivePadding = horizontalPadding > 0
-                      ? horizontalPadding
-                      : 0;
-
-                  return SliverPadding(
-                    padding: EdgeInsets.symmetric(horizontal: effectivePadding),
-                    sliver: DecoratedSliver(
-                      decoration: BoxDecoration(
-                        color: theme.colorScheme.surfaceContainer,
+                  slivers: [
+                    SliverToBoxAdapter(
+                      child: IntroductionModule(
+                        introductionData: user.introductionData,
+                        externalLinks: user.externalLinks,
                       ),
-                      sliver: SliverPadding(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: AppSizes.moduleInnerVerticalPadding,
-                          horizontal: AppSizes.moduleInnerHorizontalPadding,
-                        ),
-                        sliver: SliverMainAxisGroup(
-                          slivers: [
-                            const SliverToBoxAdapter(
-                              child: Column(
-                                children: [
-                                  ProjectsHeader(),
-                                  SizedBox(height: AppSizes.p60),
-                                ],
-                              ),
+                    ),
+                    const SliverToBoxAdapter(child: ModulesLink()),
+                    SliverToBoxAdapter(
+                      child: EducationModule(educationData: user.educationData),
+                    ),
+                    const SliverToBoxAdapter(child: ModulesLink()),
+                    SliverLayoutBuilder(
+                      builder: (context, constraints) {
+                        return DecoratedSliver(
+                          decoration: BoxDecoration(
+                            color: theme.colorScheme.surfaceContainer,
+                          ),
+                          sliver: SliverPadding(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: AppSizes.moduleInnerVerticalPadding,
+                              horizontal: AppSizes.moduleInnerHorizontalPadding,
                             ),
-                            ProjectsSliverGrid(projects: user.projects),
-                          ],
-                        ),
+                            sliver: SliverMainAxisGroup(
+                              slivers: [
+                                const SliverToBoxAdapter(
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      ProjectsHeader(),
+                                      SizedBox(height: AppSizes.p60),
+                                    ],
+                                  ),
+                                ),
+                                ProjectsSliverGrid(projects: user.projects),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    const SliverToBoxAdapter(child: ModulesLink()),
+                    SliverToBoxAdapter(
+                      child: ContactModule(
+                        email: user.email,
+                        externalLinks: user.externalLinks.reversed.toList(),
                       ),
                     ),
-                  );
-                },
-              ),
-              SliverToBoxAdapter(
-                child: Center(
-                  child: ConstrainedBox(
-                    constraints: const BoxConstraints(
-                      maxWidth: AppSizes.moduleMaxWidth,
-                      minWidth: AppSizes.moduleMinWidth,
-                    ),
-                    child: const ModulesLink(),
-                  ),
+                  ],
                 ),
               ),
-              SliverToBoxAdapter(
-                child: Center(
-                  child: ConstrainedBox(
-                    constraints: const BoxConstraints(
-                      maxWidth: AppSizes.moduleMaxWidth,
-                      minWidth: AppSizes.moduleMinWidth,
-                    ),
-                    child: ContactModule(
-                      email: user.email,
-                      externalLinks: user.externalLinks.reversed.toList(),
-                    ),
-                  ),
-                ),
+            ),
+            if (_showAnimation) // to unmount the animations once it's done
+              const IntroAnimation(
+                rectWidth: AppSizes.introAnimationRectWidth,
+                duration: _animationDuration,
               ),
-            ],
-          ),
+          ],
         ),
-        if (_showAnimation) // to unmount the animations once it's done
-          const IntroAnimation(
-            rectWidth: AppSizes.introAnimationRectWidth,
-            duration: _animationDuration,
-          ),
-      ],
+      ),
     );
   }
 }
