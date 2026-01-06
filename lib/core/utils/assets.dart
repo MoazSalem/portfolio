840d13be7669dce5914df788bf2a0cf9a3fd847c
Moaz Talaat <moaztalaat6@gmail.com>
2026-01-06 13:31:27 +0200

    fix svgs on hosted web page

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -2,7 +2,7 @@ class AppAssets {
   const AppAssets._();
 
   // Svgs
-  static const String _svgBase = "images/svgs";
+  static const String _svgBase = "assets/images/svgs";
 
   static String getSvgByName(String name) {
     return "$_svgBase/$name.svg.vec";
